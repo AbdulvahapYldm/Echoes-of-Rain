aef008b771582835670e274adfb28f3d4f827efe
Abdulvahap YILDIRIM <149504210+AbdulvahapYldm@users.noreply.github.com>
2025-08-01 13:19:54 +0100

    Define structured Gameplay Tags for items (Equipment, Consumables, Craftables)

---
+++ Plugins/Inventory/Source/Inventory/Public/Items/Inv_ItemTags.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace GameItems 
+{
+
+	namespace Equipment
+	{
+		namespace Weapons
+		{
+			UE_DECLARE_GAMEPLAY_TAG_EXTERN(Axe);
+			UE_DECLARE_GAMEPLAY_TAG_EXTERN(Sword);
+
+		}
+		namespace Cloaks
+		{
+
+		}
+		namespace Masks
+		{
+
+		}
+	}
+	namespace Consumables
+	{
+		namespace Potions
+		{
+			namespace Red
+			{
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(Small);
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(Large);
+			}
+			namespace Blue
+			{
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(Small);
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(Large);
+			}
+		}
+		
+	}
+	namespace Craftables
+	{
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(FireFernFruit);
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(LumenDais);
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(ScorchPetalBlosrom);
+	}
+
+
+}
+
+
+
